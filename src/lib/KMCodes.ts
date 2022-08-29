fa50ee7d8f7ad6f29263b169a94bb029514b92d6
Muhammad Rafat Kurkar <84540534+KMKINGMAN@users.noreply.github.com>
2022-08-29 06:14:50 +0300

    make MessagesAmout method

--- src/lib/KMCodes.ts
+++ src/lib/KMCodes.ts
@@ -7,6 +7,26 @@ class KINGMAN_ACCOUNT_BACKUPS {
         this.token = token;
         this.headers = { headers: { authorization: token, "content-type": "application/json" } };
     };
+    async MessagesAmoute(channelID: string){
+        return new Promise<number>(async(resolve, reject) => {            
+            try {
+                let Req = await axios.get(`${this.api}/channels/${channelID}/messages/search?min_id=88392440217600000`, this.headers).catch(e=> { return e });
+                if(Req.status == 202){
+                    await this.sleep(Req.data.retry_after * 1000);
+                    Req = await axios.get(`${this.api}/channels/${channelID}/messages/search?min_id=88392440217600000`, this.headers).catch(e=> { return e });
+                }
+                if(Req.status !== 200){
+                    if(Req.status === 429){
+                        await this.sleep((Req.data.retry_after * 1000) * 2);
+                        Req = await axios.get(`${this.api}/channels/${channelID}/messages/search?min_id=88392440217600000`, this.headers).catch(e=> { return e });
+                    }
+                }
+                return resolve(Req.data ? Req.data.total_results ? Req.data.total_results: -1 : -1);
+            } catch (error) {
+                return reject(error)
+            }
+        })
+    };
     async sleep(ms: number){
         return new Promise((resolve) => setTimeout(resolve, ms));
     };
