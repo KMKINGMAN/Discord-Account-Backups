dfefb499fb1211dcdf1e377526f53e38d4de0633
Muhammad Rafat Kurkar <84540534+KMKINGMAN@users.noreply.github.com>
2022-08-29 08:49:34 +0300

    log my logo and make project ask about account token

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import { mkdirSync, writeFileSync, existsSync } from "fs";
 import { KINGMAN_ACCOUNT_BACKUPS } from "./lib/KMCodes";
-import { Bar, Presets, SingleBar } from "cli-progress"
-let token = "";
+import { Bar, Presets, SingleBar } from "cli-progress";
+import { instagram } from "gradient-string";
+import inquirer from "inquirer";
+import chalk from 'chalk';
+import figlet from "figlet";
+const askToken = async() => {
+    const token = await inquirer.prompt({
+        name: "account_token",
+        type: "input",
+        message: "Enter a vaild Account Token: ",
+        default() {
+            return ""
+        },
+    });
+    return token.account_token;
+}
 (async()=> {
+    console.log(instagram(figlet.textSync("KINGMAN", "ANSI Shadow")));
+    console.log(chalk.green("KINGMAN Account's Backup | Developer Muhammad Rafat Kurakr | https://discord.gg/kmcodes\n"));
+    let token = await askToken();
+    if(token == ""){
+        return console.log(chalk.red("Invaild token pls Restart the application"));
+    }
     let backup = new KINGMAN_ACCOUNT_BACKUPS(token);
     let account_data = await backup.getAccountInfo().catch(e=> { return undefined });
-    if(!account_data) return console.log("Invaild token");
+    if(!account_data) return console.log(chalk.red("Invaild token"));
     let account_name_stage1 = account_data.username + "#" + account_data.discriminator
     let account_name = account_name_stage1.split("<").join("").split(">").join("").split("\\").join("").split("/").join("").split("|").join("").split(":").join("").split("*").join("").split("\"").join("").split("?").join("")
     let account_friends = await backup.getRelationShip().catch(e=> { return {}});
@@ -13,8 +33,7 @@ let token = "";
     if(!channls) return;
     channls.shift();
     let bar = new SingleBar({
-        align: "center",
-        format: "Backup Messages | [{bar}] {percentage}% | {value}/{total}",
+        format: `${chalk.red("Createing Backup ...")} | [${chalk.blue("{bar}")}] ${chalk.green("{percentage}%")}| {value}/{total}`,
         barCompleteChar: '\u2588',
         barIncompleteChar: '\u2591',
     });
@@ -22,7 +41,9 @@ let token = "";
     if(!existsSync("./backups/")){
         mkdirSync("./backups/",{ recursive: true });
     };
-    mkdirSync(`./backups/${account_name}`);
+    if(!existsSync(`./backups/${account_name}`)){
+        mkdirSync(`./backups/${account_name}`, { recursive: true });
+    }
     writeFileSync(`./backups/${account_name}/account_data.json`, JSON.stringify(account_data, null, 4));
     writeFileSync(`./backups/${account_name}/account_friends.json`, JSON.stringify(account_friends, null, 4));
     bar.start(channls.length, 0)
@@ -41,5 +62,5 @@ let token = "";
         bar.increment();
         bar.update(size);
     }
-    bar.stop()
+    bar.stop();
 })();
