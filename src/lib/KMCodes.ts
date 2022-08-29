2d2a7197f9ec7947c32736ab1d4d07d536681b95
Muhammad Rafat Kurkar <84540534+KMKINGMAN@users.noreply.github.com>
2022-08-29 06:13:15 +0300

    make KINGMAN_ACCOUNT_BACKUPS class

---
+++ src/lib/KMCodes.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+class KINGMAN_ACCOUNT_BACKUPS {
+    private token: string;
+    private api: string = "https://discord.com/api/v9"
+    private headers: { headers: { authorization: string, "content-type": "application/json" } }
+    constructor(token: string){
+        this.token = token;
+        this.headers = { headers: { authorization: token, "content-type": "application/json" } };
+    }
+}
