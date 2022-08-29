4110e8689a6f289069967495611fc9a4097e51bc
Muhammad Rafat Kurkar <84540534+KMKINGMAN@users.noreply.github.com>
2022-08-29 06:13:44 +0300

    add sleep method

--- src/lib/KMCodes.ts
+++ src/lib/KMCodes.ts
@@ -6,5 +6,8 @@ class KINGMAN_ACCOUNT_BACKUPS {
     constructor(token: string){
         this.token = token;
         this.headers = { headers: { authorization: token, "content-type": "application/json" } };
-    }
+    };
+    async sleep(ms: number){
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    };
 }
